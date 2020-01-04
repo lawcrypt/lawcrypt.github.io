29f4d001e15cb6f106806f02decbbf107da6f619
Joseph Zhang <josephzhang999@gmail.com>
2020-01-03 20:44:22 -0800

    added algo exe

--- handle.php
+++ handle.php
@@ -1,13 +1,11 @@
 <?php
     
-    header("Location: split.html");
     $target_dir = "uploads/";
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $file_uploads = On;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));    
     // Check if file already exists
     if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
         $file_uploads = Off;
     }
     // Check file size
@@ -21,7 +19,10 @@
     // if everything is ok, try to upload file
     } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            header("Location: loading.html");
+            exec("./main -encrypt '$target_file'");
+            //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            header("Location: split.html");
         } else {
             echo "Sorry, there was an error uploading your file.";
         }
--- run.cpp
+++
@@ -1,6 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(int argc, char **argv){
-    cout << argv[1] << endl;
-}
